127bf423a727da9edfc9e5fb80ef9c3fa175082d
edaa2 <147909812+edaa2@users.noreply.github.com>
2024-01-14 19:43:30 +0100

    création

--- test.c
+++ test.c
@@ -14,7 +14,8 @@ int main(void) {
     Rectangle button3 = { 550, 200, 200, 50 };
     Rectangle button4 = { 550, 300, 200, 50 };
     Color buttonColor = ColorFromNormalized((Vector4){ 1.0f, 0.41f, 0.41f, 1.0f }); 
-
+    int arraySize = 0;
+    int* dynamicArray = NULL;
     SetTargetFPS(60);
 
     // Main project loop
@@ -36,10 +37,35 @@ int main(void) {
 
             // Draw the button text
             DrawText("Creer", button1.x + button1.width/2 - MeasureText("Creer", 20)/2, button1.y + 15, 20, BLACK);
+             // Vérification si la souris est sur le bouton créer
+            if (CheckCollisionPointRec(GetMousePosition(), buttonRect1) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            // Afficher une fenêtre pour obtenir la taille du tableau
+            char inputText[32] = "";
+
+            // Afficher une fenêtre de saisie de texte
+            GuiTextBox(Rectangle{ 100, 100, 200, 40 }, inputText, 32, false);
+            arraySize = atoi(inputText);
+
+            // Assurer que la taille est dans(1 à 9)
+            if (arraySize < 1 || arraySize > 9) {
+                arraySize = 0; 
+                DrawText("Erreur! Veuillez entrer une valeur entre 1 et 9.", 100, 150, 20, RED);
+            } else {
+                // Allouer dynamiquement un tableau de la taille spécifiée
+                dynamicArray = (int*)malloc(arraySize * sizeof(int));
+
+                // Initialiser le tableau avec des valeurs vides
+                for (int i = 0; i < arraySize; i++) {
+                    dynamicArray[i] = 0;
+                }
+            }
+
             DrawText("Remplir", button2.x + button2.width/2 - MeasureText("Remplir", 20)/2, button2.y + 15, 20, BLACK);
             DrawText("Supprimer", button3.x + button3.width/2 - MeasureText("Supprimer", 20)/2, button3.y + 15, 20, BLACK);
-            DrawText("Ajouter", button4.x + button4.width/2 - MeasureText("Ajouter", 20)/2, button4.y + 15, 20, BLACK);
-
+            DrawText("Ajouter", button4.x + button4.width/2 - MeasureText("Ajouter", 20)/2, button4.y + 15, 20, BLACK);P
+           
+       
+        
         EndDrawing();
     }
 
