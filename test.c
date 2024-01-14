e8378c369105e5fa809eae74e6df4def270a7d12
Gherib Djeffal Lyna <155378015+lynagd@users.noreply.github.com>
2024-01-14 18:44:43 +0100

    compiled the code

--- test.c
+++ test.c
@@ -1,46 +1,50 @@
 #include "raylib.h"
 
-int main(void) { // Initialisation const int screenWidth = 800; const int screenHeight = 450;
+int main(void) {
+    // Initialisation
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-InitWindow(screenWidth, screenHeight, "Raylib Buttons");
-Color backgroundColor = ColorFromNormalized((Vector4){ 1.0f, 0.96f, 0.88f, 1.0f });
-// Define the buttons
-Rectangle button1 = { 50, 200, 200, 50 };
-Rectangle button2 = { 50, 300, 200, 50 };
-Rectangle button3 = { 550, 200, 200, 50 };
-Rectangle button4 = { 550, 300, 200, 50 };
-Color buttonColor = ColorFromNormalized((Vector4){ 1.0f, 0.41f, 0.41f, 1.0f }); 
-SetTargetFPS(60);
+    InitWindow(screenWidth, screenHeight, "Raylib Buttons");
+    Color backgroundColor = ColorFromNormalized((Vector4){ 1.0f, 0.96f, 0.88f, 1.0f });
 
-// Main project loop
-while (!WindowShouldClose())
-{
-    // Draw
-    BeginDrawing();
+    // Define the buttons
+    Rectangle button1 = { 50, 200, 200, 50 };
+    Rectangle button2 = { 50, 300, 200, 50 };
+    Rectangle button3 = { 550, 200, 200, 50 };
+    Rectangle button4 = { 550, 300, 200, 50 };
+    Color buttonColor = ColorFromNormalized((Vector4){ 1.0f, 0.41f, 0.41f, 1.0f }); 
 
-        ClearBackground(backgroundColor);
+    SetTargetFPS(60);
 
-        // Draw the comment
-        DrawText("Voulez vous creer un tableau?", screenWidth/2 - MeasureText("Voulez vous creer un tableau?", 20)/2, 50, 20, RAYWHITE);
+    // Main project loop
+    while (!WindowShouldClose())
+    {
+        // Draw
+        BeginDrawing();
 
-        // Draw the buttons
-        DrawRectangleRec(button1, buttonColor);
-        DrawRectangleRec(button2, buttonColor);
-        DrawRectangleRec(button3, buttonColor);
-        DrawRectangleRec(button4, buttonColor);
+            ClearBackground(backgroundColor);
 
-        // Draw the button text
-        DrawText("Creer", button1.x + button1.width/2 - MeasureText("Creer", 20)/2, button1.y + 15, 20, RAYWHITE);
-        DrawText("Remplir", button2.x + button2.width/2 - MeasureText("Remplir", 20)/2, button2.y + 15, 20, RAYWHITE);
-        DrawText("Supprimer", button3.x + button3.width/2 - MeasureText("Supprimer", 20)/2, button3.y + 15, 20, RAYWHITE);
-        DrawText("Ajouter", button4.x + button4.width/2 - MeasureText("Ajouter", 20)/2, button4.y + 15, 20, RAYWHITE);
+            // Draw the comment
+            DrawText("Voulez vous creer un tableau?", screenWidth/2 - MeasureText("Voulez vous creer un tableau?", 20)/2, 50, 20, BLACK);
 
-    EndDrawing();
-}
+            // Draw the buttons
+            DrawRectangleRec(button1, buttonColor);
+            DrawRectangleRec(button2, buttonColor);
+            DrawRectangleRec(button3, buttonColor);
+            DrawRectangleRec(button4, buttonColor);
 
-// De-Initialization
-CloseWindow();
+            // Draw the button text
+            DrawText("Creer", button1.x + button1.width/2 - MeasureText("Creer", 20)/2, button1.y + 15, 20, BLACK);
+            DrawText("Remplir", button2.x + button2.width/2 - MeasureText("Remplir", 20)/2, button2.y + 15, 20, BLACK);
+            DrawText("Supprimer", button3.x + button3.width/2 - MeasureText("Supprimer", 20)/2, button3.y + 15, 20, BLACK);
+            DrawText("Ajouter", button4.x + button4.width/2 - MeasureText("Ajouter", 20)/2, button4.y + 15, 20, BLACK);
 
-return 0;
+        EndDrawing();
+    }
 
+    // De-Initialization
+    CloseWindow();
+
+    return 0;
 }
